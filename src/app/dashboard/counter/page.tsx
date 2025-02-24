442209c497a0418618488fdaafba92a87edf1efb
Enrique Cerioni <enriquecerioni39@gmail.com>
2025-02-24 17:10:43 -0300

    [feat]: counter functionality added

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,9 +1,16 @@
+import { CartCounter } from "@/app/shopping-cart/components";
+
+export const metadata = {
+ title: 'Shopping Cart',
+ description: 'A simple counter',
+};
 
 
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Cart counter</span>
+      <CartCounter value={ 20 }/>
     </div>
   );
 }
